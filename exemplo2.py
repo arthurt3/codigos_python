183fd7879a447cace87b10cf17c7ecc0c9e70b34
Victor Arthur <arthurtelesfreitas2006@gmail.com>
2024-09-03 11:28:30 -0300

    novas ativides 03/09

--- exemplo2.py
+++ exemplo2.py
@@ -3,3 +3,9 @@ end = input("Informe seu endereço: ")
 idade = input("Informe sua idade: ")
 
 print(nome, end , idade)
+
+print("Ola ", nome, " seu endereço é" , end, " sua idade é ", idade)
+
+print("\nOla {} seu endereço é {} e sua idade é {}" .format(nome, end,idade))
+
+print(f"\nBem Vind@ {nome}, você mora na {end} e tem {idade} anos")
---
+++ exemplo3.py
@@ -0,0 +1,8 @@
+ 
+media = (note1 + nota2 + nota3)/3
+
+print(f"Sua média é {media}")
+
+print(f"Sua média é {media:.2f"} /3")#
+
+
