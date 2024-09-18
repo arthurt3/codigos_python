d3b0008d948daa7411478cee6f5f8663e7211cda
Victor Arthur <arthurtelesfreitas2006@gmial.com>
2024-09-18 09:40:21 -0300

    Adicionando estudo sobre função no dia 18/09

--- dicionario.py
+++ dicionario.py
@@ -21,3 +21,27 @@ print(dados_pessoais.values())
 #Exibindo as chaves utilizando o comando items()
 
 print(dados_pessoais.items())
+
+#Exibindo o dicionario detalhado
+
+for chave, valor in dados_pessoais.items():
+    print(f"{chave} = {valor}")
+
+#Realizando operações  com dicionários
+#Adcionando dados ao dicionário
+#Forma  1
+dados_pessoais["Pesos"] = 68
+print(dados_pessoais)
+
+#Forma 2 - usando o comando update
+dados_pessoais.update({"Profissão: Engenheiro"})
+print(dados_pessoais)
+
+#remover dados do dicionário
+#Forma 1 - usando o comando pop()
+dados_pessoais.pop("idade")# preciso passar a chave para podeer excluir o registro
+print(dados_pessoais)
+
+#Forma 2 - usando del()
+del(dados_pessoais["endereco"])
+print(dados_pessoais)
---
+++ funcao.py
@@ -0,0 +1,15 @@
+#Função com retorno
+def somar (num1, num2):
+    resultado = num1 + num2
+
+    return  resultado# devolvendo o valor para quem chamou a função 
+# Chamando a função
+print(somar(5, 5)) # 10
+
+#Função sem retorno
+def multiplicar (num1, num2):
+    resultado = num1 * num2
+    print(resultado)
+
+# Chamando a função
+multiplicar(2,6)
